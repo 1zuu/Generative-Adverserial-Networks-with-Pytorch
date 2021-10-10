5f8e5cd60053d45a8a2454037d76a6f9ce0dce8b
1zuu <isurualagiyawanna9717@gmail.com>
2021-10-10 10:03:29 +0530

    WGAN model

---
+++ WGAN with MNIST/variables.py
@@ -0,0 +1,17 @@
+z_dim = 64
+image_dim=28
+image_chan=1
+hidden_dim=64
+
+n_epochs = 50
+batch_size = 128
+display_step = 50
+
+beta_1 = 0.5 
+epsilon = 0.3
+c_lambda = 10
+beta_2 = 0.999
+learning_rate = 0.0002
+
+seed = 42
+crit_repeats = 5
